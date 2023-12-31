de4a4ce645d62bfbf65106e31cfeac322ec340e6
Mark Peristy <mark.peristy@gmail.com>
2024-01-01 10:05:44 +1100

    Commented out import numpy as not used.

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
-import numpy as np
+# Import numpy included in project stating template but no used in this program. 
+#import numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
